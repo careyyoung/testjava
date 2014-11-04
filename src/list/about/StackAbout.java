0dac64b476ea86fddcf0d54483dd331daa822db1
careyyoung <382094632@qq.com>
2014-11-04 17:58:50 +0800

    commit

--- src/list/about/StackAbout.java
+++ src/list/about/StackAbout.java
@@ -5,13 +5,14 @@ import java.util.Stack;
 public class StackAbout {
 
 	public static void main(String[] args) {
-		String str1 = new String("+U+n+c---+e+r+t---+a-+i-+n+t+y---+-+r+u--+l+e+s---");
-		
+		String str1 = new String(
+				"+U+n+c---+e+r+t---+a-+i-+n+t+y---+-+r+u--+l+e+s---");
+
 		Stack<Character> sta1 = new Stack<Character>();
-		for(int i=0;i<str1.length();i++){
-			if('+' == str1.charAt(i) ){
-				sta1.push( str1.charAt(i+1));
-			}else if ('-' == str1.charAt(i)) {
+		for (int i = 0; i < str1.length(); i++) {
+			if ('+' == str1.charAt(i)) {
+				sta1.push(str1.charAt(i + 1));
+			} else if ('-' == str1.charAt(i)) {
 				System.out.print(sta1.pop());
 			}
 		}
